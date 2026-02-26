4d0399a2ca2cbd7cd0517a173a6abbfaeb304995
GSerdg <grebelsergey@mail.ru>
2026-02-26 12:11:02 +0300

    feat: add motion for header

--- src/layouts/Header/Header.tsx
+++ src/layouts/Header/Header.tsx
@@ -1,17 +1,20 @@
+'use client';
+
 import { FC } from "react";
 import { HeaderLogo, HeaderMenu, HeaderNavigation } from "./components";
 import styled from "styled-components";
+import { motion } from "framer-motion";
 
-const Container = styled.div`
+const MotionContainer = styled(motion.div)`
   display: flex;
   align-items: center;
   gap: 6.6rem;
   height: 14.5rem;
 
   @media screen and (max-width: 1024px) {
-      height: 10.4rem;
-      gap: 4rem;
-    }
+    height: 10.4rem;
+    gap: 4rem;
+  }
 
   & > .header-navigation {
     @media screen and (max-width: 600px) {
@@ -22,12 +25,23 @@ const Container = styled.div`
 
 export const Header: FC = () => {
   return (
-    <Container>
+    <MotionContainer
+      initial={{ y: -100, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      transition={{
+        delay: 0.05,
+        duration: 0.6,
+        type: "spring",
+        stiffness: 100,
+        damping: 15,
+        mass: 1,
+      }}
+    >
       <HeaderLogo />
       <HeaderNavigation />
       <HeaderMenu>
         <HeaderNavigation />
       </HeaderMenu>
-    </Container>
+    </MotionContainer>
   );
 };
